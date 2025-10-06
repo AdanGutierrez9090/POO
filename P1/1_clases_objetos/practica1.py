6bb6bee9a58ee8a76a1e7563bbeade5eec9714ff
Adan Gutierrez <adan_3141240188@utd.edu.mx>
2025-10-05 22:57:16 -0600

    Add files via upload

---
+++ P1/1_clases_objetos/practica1.py
@@ -0,0 +1,16 @@
+
+
+
+class rectangulo:
+    def __init__(self,base,altura):
+        self.base = base
+        self.altura = altura
+
+    def area(self):
+        return self.base * self.altura
+    
+rect = rectangulo(5,3)
+print(rect.area())
+
+
+
