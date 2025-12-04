e993e74b48d99a068990c8584733c29b51f20a27
Adan Gutierrez <adan_3141240188@utd.edu.mx>
2025-12-03 21:12:25 -0600

    commit_03_12_25

---
+++ P3/PROYECTOS_TKINTER/coches_system/view/avances.py
@@ -0,0 +1,51 @@
+"""
+1 DICIEMBRE:
+    1) Implementacion mvc
+    2) POO
+    3) INTERFACES:
+        3.1 menu_principal()
+        3.2 menu_acciones()
+        3.3 insertar_autos()
+        3.4 consultar_autos()
+        3.5 cambiar_autos()
+        3.6 borrar_autos()
+
+    ENTREGABLES:
+        Estructura del proyecto basado en MVC
+        Modulo main
+        Interaccion con interfaces
+        Nombre del commit: "commit_01_12_25
+"""
+
+"""
+2 DICIEMBRE
+    1) INTERFACES
+        1.1 insertar_camionetas()
+        1.2 consultar_camionetas()
+        1.3 cambiar_camionetas()
+        1.4 borrar_camionetas()
+
+        2.1 insertar_camiones()
+        2.2 consultar_camiones()
+        2.3 cambiar_camiones()
+        2.4 borrar_camiones()
+
+    ENTREGABLES
+        Interaccion con todas las interfaces
+        Nombre del Commit: "commit_02_12_25"
+"""
+
+"""
+3 DICIEMBRE
+    1) CONTROLADOR
+        1.1 menu_principal()
+        1.2 menu_acciones()
+        1.3 insertar_autos()
+        1.4 consultar autos()
+        1.5 cambiar autos()
+        1.6 borrar_autos()
+
+    ENTREGABLES
+        interaccion con la funcionalidad (controlador) de las interfaces anteriores
+        nombre del commit: "commit_03_12_25"
+"""
