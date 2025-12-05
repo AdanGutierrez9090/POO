9cad2fdc415d72d5d6fe00827dad12e49c4fece1
Adan Gutierrez <adan_3141240188@utd.edu.mx>
2025-12-04 21:06:34 -0600

    commit_04_12_25

--- P3/PROYECTOS_TKINTER/coches_system/view/avances.py
+++ P3/PROYECTOS_TKINTER/coches_system/view/avances.py
@@ -49,3 +49,27 @@
         interaccion con la funcionalidad (controlador) de las interfaces anteriores
         nombre del commit: "commit_03_12_25"
 """
+
+"""
+4 Diciembre
+        1.1 insertar_camionetas()
+        1.2 consultar_camionetas()
+        1.3 cambiar_camionetas()
+        1.4 borrar_camionetas()
+
+    ENTREGABLES
+        interaccion con la funcionaliad (controlador) de las interfaces anteriores
+        nombre el commit: "commit_04_12_25"       
+"""
+
+"""
+5 Diciembre
+        1.1 insertar_camiones()
+        1.2 consultar_camiones()
+        1.3 cambiar_camiones()
+        1.4 borrar_camiones()
+
+    ENTREGABLES
+        interaccion con la funcionaliad (controlador) de las interfaces anteriores
+        nombre el commit: "commit_05_12_25"       
+"""
